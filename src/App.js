27026865f15fab14a5d71ebffb54e7000689f528
David McAfee <mcafd@amazon.com>
2022-08-05 16:20:10 -0700

    test updating id

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-import Amplify, { DataStore, Predicates } from "aws-amplify";
+import { Amplify, API, DataStore, Predicates } from "aws-amplify";
 import { Todo } from "./models";
+import * as queries from "./graphql/queries";
 
 //Use next two lines only if syncing with the cloud
 import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
 
-Amplify.Logger.LOG_LEVEL = "DEBUG";
+// Amplify.Logger.LOG_LEVEL = "DEBUG";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -35,6 +36,26 @@ function App() {
     const _todos = await DataStore.query(Todo);
     setTodos(_todos);
     console.log("Todos", _todos);
+    const allRecords = await API.graphql({ query: queries.listTodos });
+    console.log("everything in the table:", allRecords);
+  }
+
+  // Update
+  async function updateTodo() {
+    const [originalTodo] = await DataStore.query(Todo);
+    console.log("Original Todo:", originalTodo);
+
+    try {
+      const todo = await DataStore.save(
+        Todo.copyOf(originalTodo, (updated) => {
+          updated.id = `name ${Date.now()}`;
+        })
+      );
+
+      console.log("Todo updated:", todo);
+    } catch (error) {
+      console.error("Save failed:", error);
+    }
   }
 
   // console.log(msg.model, msg.opType, msg.element);
@@ -52,6 +73,7 @@ function App() {
         <div>
           <button onClick={getTodos}>Query</button>
           <input type="button" value="NEW" onClick={onCreate} />
+          <input type="button" value="UPDATE" onClick={updateTodo} />
           <input type="button" value="DELETE ALL" onClick={onDeleteAll} />
           <button onClick={() => DataStore.start()}>Start</button>
           <button onClick={() => DataStore.stop()}>Stop</button>
