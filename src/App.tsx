f604874b83fb1376d0a547f4190859e3e4675e20
David McAfee <mcafd@amazon.com>
2022-11-08 11:18:53 -0800

    restore App.tsx; upgrade Amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import Amplify, { DataStore, Predicates } from "aws-amplify";
+import { Todo } from "./models";
+
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
+
+Amplify.Logger.LOG_LEVEL = "DEBUG";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  function onCreate() {
+    DataStore.save(
+      new Todo({
+        name: `name ${Date.now()}`,
+      })
+    );
+  }
+
+  function onDeleteAll() {
+    DataStore.delete(Todo, Predicates.ALL);
+  }
+
+  async function getTodos() {
+    const _todos = await DataStore.query(Todo);
+    //@ts-ignore
+    setTodos(_todos);
+    console.log("Todos", _todos);
+  }
+
+  useEffect(() => {
+    const subscription = DataStore.observe(Todo).subscribe(() => {
+      getTodos();
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button onClick={getTodos}>Query</button>
+          <input type="button" value="NEW" onClick={onCreate} />
+          <input type="button" value="DELETE ALL" onClick={onDeleteAll} />
+          <button onClick={() => DataStore.start()}>Start</button>
+          <button onClick={() => DataStore.stop()}>Stop</button>
+          <button onClick={() => DataStore.clear()}>Clear</button>
+          <pre>todos: {JSON.stringify(todos, null, 2)}</pre>
+        </div>
       </header>
     </div>
   );
